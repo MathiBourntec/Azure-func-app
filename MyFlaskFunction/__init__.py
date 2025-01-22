7707fbb9434d555bde5119b3aaef9d5d3100c230
Mathiyarasu <116270962+MathiBourntec@users.noreply.github.com>
2025-01-22 16:50:56 +0530

    Update __init__.py

--- MyFlaskFunction/__init__.py
+++ MyFlaskFunction/__init__.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../Templates')
 
 @app.route('/')
 def home():
